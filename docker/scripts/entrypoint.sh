2371e9451a2eec808c93a724fa1e4ac5a02147f4
Erik J. Olson <hello@erikjolson.com>
2021-01-19 15:11:54 -0600

    Move symfony cache to volume to improve performance

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-cd /var/www
+APP_DIR=/var/www
+APP_GROUP=www-data
+OWD=`pwd`
 
 # update composer dependencies
+cd $APP_DIR
 composer install
+
+DIRS=( \
+    "${APP_DIR}/var/cache" \
+)
+
+for dir in ${DIRS[@]}; do
+  echo $DIR
+    mkdir --parents $dir
+    chmod 775 $dir
+    chgrp $APP_GROUP $dir
+done
+
+cd $OWD
