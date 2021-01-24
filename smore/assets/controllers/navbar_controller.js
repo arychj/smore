2ebf0ea7a7e1cc9207a0171af50116d0ecf02cae
Erik J. Olson <hello@erikjolson.com>
2021-01-24 15:21:33 -0600

    Highlight active navbad items

---
+++ smore/assets/controllers/navbar_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from 'stimulus';
+import $ from 'jquery';
+
+export default class extends Controller {
+    connect() {
+        let path = window.location.pathname;
+        let links = $(this.element).find('li>a');
+
+        $(links).each(function(i, link){
+            if($(link).attr('href') === path){
+                $(link).closest('li').addClass('active');
+            }
+        });
+    }
+}
